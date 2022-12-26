4318ea27eba9f2dc2819837da883649d12258936
messter10 <rumessa99@gmail.com>
2022-12-27 01:36:52 +0500

    basic pagination done

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,6 @@
 import '../styles/User.scss';
 import React, { useState, useEffect, useMemo } from 'react';
-import { useTable } from 'react-table';
+import { usePagination, useSortBy, useTable } from 'react-table';
 import axios from 'axios';
 
 function formatCounts(num) {
@@ -16,17 +16,20 @@ const COLUMNS = [
     {
         Header: 'Description',
         accessor: 'description',
-        Cell: props => <div className="repo-desc">{props.value}</div>
+        Cell: props => <div className="repo-desc">{props.value}</div>,
+        disableSortBy: true
     },
     {
         Header: 'Stars',
         accessor: 'stargazers_count',
-        Cell: props => <div style={{'textAlign': 'center'}}>{formatCounts(props.value)}</div>
+        Cell: props => <div style={{'textAlign': 'center'}}>{formatCounts(props.value)}</div>,
+        disableSortBy: true
     },
     {
         Header: 'Actions',
         accessor: 'html_url',
-        Cell: props => <a className="repo-btn button" href={props.value} target="_blank">Visit</a>
+        Cell: props => <a className="repo-btn button" href={props.value} target="_blank">Visit</a>,
+        disableSortBy: true
     }
 ]
 
@@ -46,39 +49,64 @@ function User(props) {
         setIsFetching(true);
         axios({
             method: "GET",
-            url: props.details.repos_url + "?per_page=" + count,
+            url: props.details.repos_url 
+            // + "?per_page=" + count,   (if we wanted a specific number per load for example)
           }
         )
         .then((res) => {
+            // initially sorted according to star count
             setRepos(
                 res.data.sort((a, b) => b.stargazers_count - a.stargazers_count)
             )
-            if (res.data.length === repos.length) {
-                setNoMore(true);
-            }
+            // if (res.data.length === repos.length) {
+            //     setNoMore(true);
+            // }
             setRepoLoaded(true);
-            setIsFetching(false);
+            // setIsFetching(false);
 
-            if (res.data.length === 0) {setCount(0);}
-            else {setCount(count + 2);}
+            // if (res.data.length === 0) {setCount(0);}
+            // else {setCount(count + 2);}
         })
         .catch((e) => {
             console.log(e);
         });
     }
 
+    function compareIgnoreCase(a, b) { 
+        let r1 = a.toLowerCase(); 
+        let r2 = b.toLowerCase(); 
+        if (r1 < r2) { return -1; } 
+        if (r1 > r2) { return 1; } 
+        return 0; 
+    }
+
     const columns = useMemo(() => COLUMNS, []);
     const data = useMemo(() => repos, [repos]);
     const tableInstance = useTable({
         columns,
-        data 
-    });
+        data,
+        sortTypes: {
+            alphanumeric: (row1, row2, columnName) => {
+                return compareIgnoreCase(row1.values[columnName], row2.values[columnName])
+            },
+        }
+    },
+    useSortBy,
+    usePagination);
     const { getTableProps, 
         getTableBodyProps, 
         headerGroups, 
-        rows, 
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        state, 
         prepareRow 
     } = tableInstance;
+
+    const { pageIndex } = state;
 
     return (
     <div className="center-div d-flex flex-column align-items-center">
@@ -119,7 +147,10 @@ function User(props) {
                         <tr {...headerGroup.getHeaderGroupProps()}>
                             {
                                 headerGroup.headers.map((column) => (
-                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        {column.render('Header')}
+                                        {column.isSorted ? (column.isSortedDesc ? " ↓" : " ↑") : null}
+                                    </th>
                                 ))
                             }
                         </tr>
@@ -129,7 +160,7 @@ function User(props) {
 
             <tbody {...getTableBodyProps()}>
                 {
-                    rows.map((row) => {
+                    page.map((row) => {
                         prepareRow(row)
                         return (
                             <tr {...row.getRowProps()}>
@@ -146,11 +177,20 @@ function User(props) {
         </table>
         }
 
-        {!isFetching && !noMore && <button className='user-repos-button'onClick={loadRepos}>Load more</button>}
+        <div className='pagination-area'>
+            <p className='page-numbers'>Page {pageIndex + 1} of {pageOptions.length}</p>
+            <div className='page-buttons'>
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
+                <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+            </div>
+        </div>
+        
+
+        {/* {!isFetching && !noMore && <button className='user-repos-button'onClick={loadRepos}>Load more</button>}
 
         {isFetching && <button className='user-repos-button'>Loading...</button>}
 
-        {count === 0 ? <p>No repositories to show</p> : noMore && <button className='user-repos-button-no-more'>No more to load</button>}
+        {count === 0 ? <p>No repositories to show</p> : noMore && <button className='user-repos-button-no-more'>No more to load</button>} */}
 
     </div>  
     );
